aa8afd21ab2624be5faa62dd6191170a40b0d167
dean <dean@realpacking.com>
2024-04-17 17:00:01 +0900

    data fetch - statelessWidget 에서 적용

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,34 +1,60 @@
+// import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+// 기초적인 data fetching 방식
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
 
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
+// class _HomeScreenState extends State<HomeScreen> {
+//   List<WebtoonModel> webtoons = [];
+//   bool isLoading = true;
 
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
+//   void waitForWebtoons() async {
+//     webtoons = await ApiService.getTodaysToons();
+//     isLoading = false;
+//     setState(() {});
+//   }
 
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     waitForWebtoons();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         title: const Text(
+//           "오늘의 웹툰",
+//           style: TextStyle(
+//             fontSize: 24,
+//             fontWeight: FontWeight.w500,
+//           ),
+//         ),
+//         foregroundColor: Colors.green,
+//         backgroundColor: Colors.white,
+//         elevation: 2,
+//       ),
+//     );
+//   }
+// }
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -42,6 +68,17 @@ class _HomeScreenState extends State<HomeScreen> {
         foregroundColor: Colors.green,
         backgroundColor: Colors.white,
         elevation: 2,
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          // snapshot: future의 상태값
+          if (snapshot.hasData) {
+            return const Text('there is data');
+          } else {
+            return const Text('Loading...');
+          }
+        },
       ),
     );
   }
